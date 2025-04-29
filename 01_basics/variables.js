161a3ed79597ed72ac8af4d75a1e12514368036e
akashborude <58294924+akashborude@users.noreply.github.com>
2025-04-29 12:57:59 +0000

    basics variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = "1234";
+let accountName = "Akash"
+var accountEmail = "akash@gmail.com"
+accountCity = "Pune";
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountName, accountCity, accountState])
