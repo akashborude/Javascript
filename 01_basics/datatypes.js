5c6474b864c36a5bb3f75964833885bfd8617a13
akashborude <58294924+akashborude@users.noreply.github.com>
2025-04-29 13:17:20 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+let name = "akash"
+let age  = 24;
+let isLoggedIn = true;
+let state;
+let value =  null;
+
+console.log(typeof name); // string
+console.log(typeof age); // int
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof state); //undefined
+console.log(typeof value); // null -> is a object
+
+
