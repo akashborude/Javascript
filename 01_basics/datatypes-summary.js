75f66afc9d3e900689d5952d7fd44deb55535eab
akashborude <58294924+akashborude@users.noreply.github.com>
2025-04-30 17:46:40 +0000

    dataypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// Two Types of Datatypes
+// 1. Primitive Datatypes
+// 2. Non - Primitive Datatypes
+
+
+// Primitive Datatypes (Also known as Call by Value)
+// 7 types: String, Number, Boolean, null, undefined, symbol & BigInt
+
+// Non - Primitive Datatypes (Also known as Call by Reference)
+// 3 types: Arrays, Objects, functions
