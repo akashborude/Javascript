cbf20ef50a38eb7c1739ee3878ade81b6c887954
akashborude <58294924+akashborude@users.noreply.github.com>
2025-04-30 18:07:37 +0000

    commit

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,5 +6,13 @@
 // Primitive Datatypes (Also known as Call by Value)
 // 7 types: String, Number, Boolean, null, undefined, symbol & BigInt
 
+const score = 100;
+console.log(score);
+
+const new_score = 120.20
+console.log(new_score);
+
+
+
 // Non - Primitive Datatypes (Also known as Call by Reference)
 // 3 types: Arrays, Objects, functions
